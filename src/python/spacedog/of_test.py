726bdd9c8b421a2bffd280e4d2a91d9698e1af2e
Adrian Mak <damarkian@gmail.com>
2020-07-03 16:39:25 +0800

    string matrix

--- src/python/spacedog/of_test.py
+++ src/python/spacedog/of_test.py
@@ -12,11 +12,26 @@ def outputjson(_teststr, jsonfile):
     with open(jsonfile, 'w') as f:
         f.write(json.dumps(_jsondict))
 
+
+def outputjson2(idict, jsonfile):
+    with open(jsonfile, 'w') as f:
+        f.write(json.dumps(idict))
+
 def of_test():
     cwd = os.path.dirname(os.path.realpath(__file__))
     moleculeFile = str(Path(cwd + "/moldata/h3_250.hdf5")) 
     molecule = of.MolecularData(filename=moleculeFile)
+
+    S = np.load(os.path.join("moldata", "overlap.npy"))
+
+
     e_nuc = molecule.load()
-    return(e_nuc)
+
+    opd = {}
+    opd["eNuc"] = str(e_nuc)
+    opd["overlapMatrix"] = np.array2string(S)
+    opd["Schema"] = "spacedog-result"
+
+    return(opd)
 
 
