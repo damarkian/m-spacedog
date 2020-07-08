a2ab0f32b7256585d114f6d1b5a7295a6d0f8158
Adrian Mak <damarkian@gmail.com>
2020-07-08 10:41:23 +0800

    added third_party

--- src/python/spacedog/of_test.py
+++ src/python/spacedog/of_test.py
@@ -6,9 +6,19 @@ import scipy.linalg
 import openfermion as of
 import json
 
+from .gradient_hf import rhf_func_generator
+from .opdm_functionals import OpdmFunctional
+from .analysis import (compute_opdm,
+                            mcweeny_purification,
+                            resample_opdm,
+                            fidelity_witness,
+                            fidelity)
+from .third_party.higham import fixed_trace_positive_projection
+
+
 from .gradient_hf import rhf_minimization
 from .objective import (RestrictedHartreeFockObjective, generate_hamiltonian)
-
+from .molecular_example_odd_qubits import make_h3_2_5
 
 def outputjson(_teststr, jsonfile):
     _jsondict = {}
@@ -17,41 +27,15 @@ def outputjson(_teststr, jsonfile):
     with open(jsonfile, 'w') as f:
         f.write(json.dumps(_jsondict))
 
-def make_h3_250() -> Tuple[RestrictedHartreeFockObjective, of.MolecularData, 
-                           np.ndarray, np.ndarray, np.ndarray]:
-    cwd = os.path.dirname(os.path.realpath(__file__))
-    h3_250_path = str(Path(cwd+ "/moldata/"))
     
 
 def of_test(jsonfile):
-    cwd = os.path.dirname(os.path.realpath(__file__))
-    moleculeFile = str(Path(cwd + "/moldata/h3_250.hdf5")) 
-    molecule = of.MolecularData(filename=moleculeFile)
-    molecule.load()
 
-    # TODO: separate all these steps to produce one dictionary entry per step
-
-    e_nuc = molecule.nuclear_repulsion
-
-    S = np.load(str(Path(cwd + "/moldata/overlap.npy")))
-    Hcore = np.load(str(Path(cwd + "/moldata/h_core.npy")))
-    TwoERI = np.load(str(Path(cwd + "/moldata/two_eri.npy")))
-
-    _, X = scipy.linalg.eigh(Hcore, S)
-
-    obi = of.general_basis_change(Hcore, X, (1, 0))
-    tbi = np.einsum('psqr', of.general_basis_change(TwoERI, X, (1, 0, 1, 0)))
-
-    hamiltonian = generate_hamiltonian(obi, tbi, molecule.nuclear_repulsion, 1.0e-12)
-
-    rhf_objective = RestrictedHartreeFockObjective(hamiltonian, molecule.n_electrons)
-
-    scipy_result = rhf_minimization(rhf_objective)
+    rhf_objective, molecule, parameters, obi, tbi = make_h3_2_5()
+    ansatz, energy, gradient = rhf_func_generator(rhf_objective)
 
     opd = {}
-    opd["eNuc"] = str(e_nuc)
-    opd["overlapMatrix"] = np.array2string(S)
-    opd["orthoX"] = np.array2string(X)
+    opd["energy"] = str(energy)
     opd["schema"] = "spacedog-result"
 
     with open(jsonfile, 'w') as f:
